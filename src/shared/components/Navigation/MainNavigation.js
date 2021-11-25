98d55ad8012a4320222e28ff61e923184e8bd715
Samarthya <samarthya55@gmail.com>
2021-11-25 17:24:36 +0530

    side drawer done properly

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,39 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import MainHeader from './MainHeader'
 import './MainNavigation.css'
 import NavLinks from './NavLinks'
 import SideDrawer from './SideDrawer'
+import Backdrop from '../../UIElements/Backdrop'
 
 const MainNavigation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    function openDrawer() {
+        setDrawerIsOpen(true);
+    }
+
+    function closeDrawer() {
+        setDrawerIsOpen(false);
+    }
+
     return (
         <>
-        <SideDrawer>
-            <nav>
-                <NavLinks />
-            </nav>
-        </SideDrawer>
-        <MainHeader>
-            <button className="main-navigation__menu-btn"><i className="bi bi-list"></i></button>
-            <h1 className="main-navigation__title"><Link to="/">MyPlaces</Link></h1>
-            <nav className="main-navigation__nav-links">
-                <NavLinks />
-            </nav>
-        </MainHeader>
+            {
+                drawerIsOpen && <Backdrop clickHandler={closeDrawer} />
+            }
+            <SideDrawer show={drawerIsOpen}>
+                <nav>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button onClick={openDrawer} className="main-navigation__menu-btn"><i className="bi bi-list"></i></button>
+                <h1 className="main-navigation__title"><Link to="/">MyPlaces</Link></h1>
+                <nav className="main-navigation__nav-links">
+                    <NavLinks />
+                </nav>
+            </MainHeader>
         </>
     )
 }
